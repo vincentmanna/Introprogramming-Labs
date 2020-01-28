48365f56af894754e4deea9576474e09c68ca7cf
Vincent Manna <60368657+vincentmanna@users.noreply.github.com>
2020-01-27 19:12:09 -0500

    Added goodbye message to Hello program

--- hello.py
+++ hello.py
@@ -4,3 +4,10 @@
 def main():
     print("Hello, instructor!")
 main()
+# Introduction to Programming
+# Author: Vincent Manna
+# Date: 1/27/2020
+def main():
+    print("Hello, instructor!")
+    print("Good-bye!")
+main()
