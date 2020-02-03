c99f89040874a74edf6da19cccd749234ee1a7a5
Vincent <Trackstar248>
2020-02-03 18:29:10 -0500

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,6 @@
+noun = input("Enter in a noun.")
+verb = input("Enter in a verb.")
+adjective = input("Enter in an adjective.")
+place = input("Enter in a place.")
+print("There was a very " + adjective + " " + noun + " that was " + verb + " across the street from my " + place)
+
