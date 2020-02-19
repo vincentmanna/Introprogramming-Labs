16c38b756f629c23edb3e5531abe0f1078dd2285
Vincent <Trackstar248>
2020-02-18 20:23:18 -0500

    More usability improvements to the guessing game. FINAL SUBMISSION

--- guessinggame.py
+++ guessinggame.py
@@ -5,26 +5,28 @@ def myname(gamename):
 def main():
     gamename = "PYTHON GUESSING GAME"
     myname(gamename)
-    print("To quit the game, type 'quit'")
+    print("To quit the game, type any word that begins with 'q'")
     print()
     print("I am thinking of a farm animal..")
-
     guess = input("Guess the animal I am thinking of! ")
     answer = "pig"
-    quitt = "quit"
+    quitt = "q"
     x = not answer
-
-    while guess != x:
-        print("Sorry. You guessed incorrectly. Guess again..")
-        guess = input("Guess the animal I am thinking of! ")
-
+    while True:
         if guess == answer:
-            print("Congratulations! You guessed it!")
+            y = input("Congratulations! You guessed it! Do you like it? Enter 'y' or 'n'! ")
+            if y == "y":
+                print("I am happy you like the animal!")
+                break
+            elif y == "n":
+                print("Thats unfortunate. I am sorry you dislike the animal!")
+                break
+        elif guess[0] == quitt:
+            print("The game is over.")
             break
-
-        elif guess == quitt:
-            print("The game is over")
-            break
+        else:
+            print("You are incorrect. ")
+            guess = input("Try and guess the animal again! ")
 
 
 main()
