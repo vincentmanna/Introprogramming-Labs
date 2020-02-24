c3e27d47b07f47a4760ba7c3bd07c1def50b0559
Vincent <Trackstar248>
2020-02-24 18:22:31 -0500

    'a modular design' commit lab 5

--- credentials.py
+++ credentials.py
@@ -3,25 +3,43 @@
 # Author: Vincent Manna
 # Created: 2020-02-24
 
-def main():
+def myname():
     # get user's first and last names
     first = input("Enter your first name: ")
     last = input("Enter your last name: ")
-    #  modify this to generate a Marist-style username
-    uname = first + '.' + last
-    print(uname)
-    # ask user to create a new password
+    return first, last
+
+
+def maristname():
+    mar1 = 'Your marist username is: '
+    mar2 = '@marist.edu'
+    return mar1, mar2
+
+
+###
+def mypassword():
     passwd = input("Create a new password: ")
     #  modify this to ensure the password has at least 8 characters
 
     while True:
         if len(passwd) >= 8:
-            print("The force is strong in this one…")
-            print("Account configured. Your new email address is " + uname + "@marist.edu")
+            print("Your password is strong!")
+            print("Your password is: " + passwd)
             break
         else:
-            print("Fool of a Took! That password is feeble!")
+            print("Your password is weak! Must be 8 characters or more.")
             passwd = input("Create a new password: ")
+
+    return passwd
+
+
+def main():
+    first, last = myname()
+    print(first, last)
+    mar1, mar2 = maristname()
+    print(mar1 + first + '.' + last + mar2)
+    passwd = mypassword()
+    print(passwd)
 
 
 main()
